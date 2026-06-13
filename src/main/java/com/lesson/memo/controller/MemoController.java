a0b906075c66f9060be95e79a8bd3904ba692dd0
Ayan Wakk <ayami.k1990.0904@gmail.com>
2026-06-13 16:40:41 +0900

    全文検索機能の実装

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
@@ -29,13 +30,35 @@ public class MemoController {
 
     @Autowired
     private MemoRepository memoRepository;
-
+    
     @GetMapping
     public String list(Model model) {
-        List<Memo> memos = memoRepository.findAll();
+    	
+    	List<Memo> memos = memoRepository.findAll();
+        
+    memos.sort(Comparator.comparing(Memo::getPriority));
+ 
+    model.addAttribute("memos", memos);
+    return "memo-list";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(required = false) String keyword,
+    		           Model model) {
+    	
+    	List<Memo> memos;
+    	
+    	if (keyword == null || keyword.isBlank()) {
+    		memos = memoRepository.findAll();
+    	} else {
+    		memos = memoRepository.findByTitleContainingOrContentContaining(
+    	    		keyword,
+    	            keyword);
+    	}
         
         memos.sort(Comparator.comparing(Memo::getPriority));
-        
+ 
+        model.addAttribute("keyword", keyword);
         model.addAttribute("memos", memos);
         return "memo-list";
     }
